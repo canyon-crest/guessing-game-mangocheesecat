69588bcbe154a9009877d492e05f5bf4d6e819fa
mangocheesecat <jic7827@my.sduhsd.net>
2026-04-10 06:09:30 +0000

    11

--- script.js
+++ script.js
@@ -26,6 +26,9 @@ function time() {
     let year = d.getFullYear();
     let suffix = "th";
     if (day % 10 == 1 && day !=11) suffix = "st";
+    else if (day % 10 == 2 && day !=12) suffix = "nd";
+    else if (day % 10 == 3 && day !=13) suffix = "rd";
+    return month + " " + day + suffix + ", " + year + " " + d.getHours() + ":" + String(d.getMinutes()).padStart(2, "0") + ":" + String(d.getSeconds()).padStart(2, "0");
 }
 
 function play(){
